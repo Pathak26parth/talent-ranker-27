a423d75a7c322beeba1e1628936a8b3f5d5d118f
Parth Pathak <121369858+Pathak26parth@users.noreply.github.com>
2025-08-30 04:45:45 +0530

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,9 +99,7 @@ export default {
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out'
-			}
-		}
-	},
+			},
 			boxShadow: {
 				'primary': 'var(--shadow-primary)',
 				'card': 'var(--shadow-card)',
